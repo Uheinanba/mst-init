3c158770a28695581a9b000764eae20329ee56bf
yuank <yuank@fxiaoke.com>
2018-03-09 16:27:24 +0800

    update invoive

--- src/invoice/models/invoice.js
+++ src/invoice/models/invoice.js
@@ -1,7 +1,24 @@
 import {types} from 'mobx-state-tree'
 import ItemList from './itemList';
 
+const Box = types.model("Box", {
+    width: types.number,
+    height: types.number
+})
+const Square = types.model("Square", {
+    width: types.number
+})
+
+// todo
+/* const Plane = types.union(Square, Box)
+const DispatchPlane = types.union(
+    snapshot => (snapshot && "height" in snapshot ? Box : Square),
+    Box,
+    Square
+)
+ */
 const Invoice = types.model('Invoice', {
+    // plane: types.optional(Plane, { width: 3 }),
     currency: types.string,
     is_paid: false,
     itemList: types.optional(ItemList, {items: []}),
